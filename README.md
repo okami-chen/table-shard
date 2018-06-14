f094fc973c18b506c24898bc8532ed2847f8b4d3
陈德华 <chendehua@enmonster.org>
2018-06-14 10:44:24 +0800

    增加例子

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TableShard
 Laravel Table Sharding
+
+```php
+
+use Illuminate\Database\Eloquent\Model;
+
+use OkamiChen\TableShard\Traits\TableShard;
+
+
+class User extends Model {
+    
+    use TableShard;
+
+}
+
+```
--- src/Traits/TableShard.php
+++ src/Traits/TableShard.php
@@ -2,10 +2,20 @@
 
 namespace OkamiChen\TableShard\Traits;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 trait TableShard {
     
+    /**
+     * 分表数量,最好为8的倍数
+     * @var int 
+     */
     public $shardNum    = 8;
     
+    /**
+     * 根据那个表取余
+     * @var string 
+     */
     public $shardKey    = 'id';
     
     /**
@@ -17,6 +27,27 @@ trait TableShard {
         $value  = $this->$key;
         return $this->table.'_'. sprintf('%02d', $value % $this->shardNum);
     }
-    
+        
+    /**
+     * Define a one-to-one relationship.
+     *
+     * @param  string  $related
+     * @param  string  $foreignKey
+     * @param  string  $localKey
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function hasOne($related, $foreignKey = null, $localKey = null)
+    {
+        $model          = new $related();
+        $tableName      = $this->getShardTable();
+        $model->setTable($tableName);
+
+        $foreignKey = $foreignKey ?: $this->getForeignKey();
+
+        $localKey = $localKey ?: $this->getKeyName();
+        
+        return new HasOne($model->newQuery(), $this, $foreignKey, $localKey);
+        //return new HasOne($model->newQuery(), $this, $model->getTable().'.'.$foreignKey, $localKey);
+    }
 }
 
